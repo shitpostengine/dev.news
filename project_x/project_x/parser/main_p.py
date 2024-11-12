f1ad747f37c1736425a0c54c1d1dea37cafd4a25
required <gleb.krasnow@ya.ru>
2024-11-12 21:16:23 +0300

    tproger async parsing with scrolling pages

--- project_x/project_x/parser/main_p.py
+++ project_x/project_x/parser/main_p.py
@@ -1,41 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+import datetime
 
-whitelist = [
-    'https://habr.com',
-    'https://tproger.ru'
-]
+now = datetime.datetime.now()
+URL = 'https://tproger.ru'
 
-keywords = ['Python', 'Django', 'парсинг', 'Питон', 'Джанго']
-
-
-def parse_site(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Search for the articles differently depending on site structure
-    for article in soup.find_all('h2'):
-        title = article.get_text()
-        if any(keyword.lower() in title.lower() for keyword in keywords):
-            # Check parent tag for article content
-            article_parent = article.find_parent('article') or article.find_parent(
-                'div')  # Try div if article not found
-            if article_parent:
-                paragraphs = article_parent.find_all('p')  # Find all paragraph tags
-                if not paragraphs:
-                    paragraphs = article_parent.find_all('div')  # If no <p>, try <div> as fallback
-
-                article_text = ' '.join(paragraph.get_text() for paragraph in paragraphs)
-                demo = ' '.join(article_text.split()[:200])  # Limit the text to 200 words for demo
-
-                # Extract the article URL
-                article_url = article.find('a')['href'] if article.find('a') else url
-                article_url = urljoin(url, article_url)  # Ensure the URL is absolute
-
-                print(f'Найдена статья: {title} на {article_url}')
-                print(f'title: {title}, demo: {demo}\n')
-
-
-for site in whitelist:
-    parse_site(site)
+HEADERS={
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.206'
+}
+r=requests.get(URL,headers=HEADERS)
+if r.status_code == 200:
+	page = r.text
+	soup = BeautifulSoup(page, 'html.parser')
+	items = soup.find_all('a',class_="article-link")
+	i=0
+	data=[]
+	for j in items:
+		data.append(items[i].get('href'))
+		# 'title': item.find('a',class_='c-events__name').get_text(strip=True),
+		# print('link',item.find('a',class_='article-link').get('href'))
+		i+=1
+	with open('data.txt', 'a+', encoding='utf-8') as g:
+		g.write(f'данные с сайта на сегодня( {str(now)}) {data}\n-----')
